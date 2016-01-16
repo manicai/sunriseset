6ef144a12bdce84a8d768dec33b53df33df647a4
Ian Glover <ian.glover@gmail.com>
2016-01-16 14:13:34 +0000

    Update time download script to gather a date range and include python module requirements.

--- extract.py
+++ extract.py
@@ -6,14 +6,7 @@ import sys
 def extract_cell_time(cell_text):
     return cell_text.split()[0]
 
-def main():
-    if len(sys.argv) != 3:
-        print >> sys.stderr, 'Usage: extract <month> <year>'
-        sys.exit(1)
-
-    month = sys.argv[1]
-    year = sys.argv[2]
-
+def dump_month(month, year):
     url = ('http://www.timeanddate.com/sun/uk/london?month=%s&year=%s' %
            (month, year))
 
@@ -37,7 +30,32 @@ def main():
         sunrise = extract_cell_time(row.find_all('td')[0].text)
         sunset = extract_cell_time(row.find_all('td')[1].text)
 
-        print day, month, year, sunrise, sunset
+        print "%s,%s,%s,%s,%s" % (day, month, year, sunrise, sunset)
+
+
+def main():
+    if len(sys.argv) != 3 and len(sys.argv) != 5:
+        print >> sys.stderr, 'Usage: extract <month> <year> <end_month> <end_year>'
+        print >> sys.stderr, '  end_month and end_year are optional.'
+        sys.exit(1)
+
+    month = int(sys.argv[1])
+    year = int(sys.argv[2])
+
+    if len(sys.argv) == 5:
+        end_month = int(sys.argv[3])
+        end_year = int(sys.argv[4])
+    else:
+        end_month = month
+        end_year = year
+
+    print "Day,Month,Year,Sunrise,Sunset"
+    while year < end_year or (year == end_year and month <= end_month):
+        dump_month(month, year)
+        month += 1
+        if month == 13:
+            month = 1
+            year += 1
 
 if __name__ == '__main__':
     main()
